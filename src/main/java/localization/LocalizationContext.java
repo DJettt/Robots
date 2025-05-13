a49617356328603e9e6f24b8161e71f43af39db4
Artemchik <ddgett08@gmail.com>
2025-05-13 13:12:03 +0500

    Добавил Java-Doc-и в класс LocalizationContext.

--- src/main/java/localization/LocalizationContext.java
+++ src/main/java/localization/LocalizationContext.java
@@ -8,6 +8,9 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+/**
+ * Отвечает за локализацию приложения.
+ */
 public class LocalizationContext implements Savable {
     private final static String PREFIX = "local";
 
@@ -18,33 +21,61 @@ public class LocalizationContext implements Savable {
     private ResourceBundle bundle;
     private final List<LocalizationListener> listeners = new ArrayList<>();
 
+    /**
+     * Конструктор.
+     */
     public LocalizationContext() {
     }
 
+    /**
+     * Устанавливает текущую локализацию.
+     * @param locale Новая локализация.
+     */
     public void setLocale(Locale locale) {
         this.currentLocale = locale;
         this.bundle = ResourceBundle.getBundle("Messages", locale);
         notifyListeners();
     }
 
+    /**
+     * Получает строку из ResourceBundle
+     * @param key Ключ строки в файле.
+     * @return Строка для вывода.
+     */
     public String getString(String key) {
         return bundle.getString(key);
     }
 
+    /**
+     * Возвращает текущую локализацию.
+     * @return Текущая локализация.
+     */
     public Locale getCurrentLocale() {
         return currentLocale;
     }
 
+    /**
+     * Добавляет слушателя.
+     * @param listener Слушатель локализации.
+     */
     public void addListener(LocalizationListener listener) {
         listeners.add(listener);
     }
 
+    /**
+     * Уведомляет слушателей об изменениях.
+     */
     private void notifyListeners() {
         for (LocalizationListener listener : listeners) {
             listener.onLanguageChanged();
         }
     }
 
+    /**
+     * Устанавливает параметры.
+     * @param params Параметры, которые нужно установить
+     *               (в данном случае только 1 параметр)
+     */
     public void setParams(Map<String, String> params) {
         setLocale(Locale.forLanguageTag(params.get(LOCALE)));
     }
